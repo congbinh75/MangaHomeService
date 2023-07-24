4bb2e0e9c93d8cf67eae724f51a1a1c3dbda1417
congbinh75 <dangcongbinh36@gmail.com>
2023-07-24 11:18:32 +0700

    Re-initialize migration

---
+++ Models/Group/Group.cs
@@ -0,0 +1,9 @@
+﻿namespace MangaHomeService.Models.Group
+{
+    public class Group
+    {
+        public string Name { get; set; }
+        public User Leader { get; set; }
+        public List<User> Members { get; set; }
+    }
+}
