1c7d6f3c133dbbdbcc7a68846d03a64aa75eca33
congbinh75 <dangcongbinh36@gmail.com>
2023-10-25 16:44:59 +0700

    Add PageService

--- Services/Interfaces/IPageService.cs
+++ Services/Interfaces/IPageService.cs
@@ -4,5 +4,10 @@ namespace MangaHomeService.Services.Interfaces
 {
     public interface IPageService
     {
+        public Task<Page> Get(string id);
+        public Task<List<Page>> GetByChapter(string chapterId);
+        public Task<Page> Add(string chapterId, int number, IFormFile file);
+        public Task<Page> Update(string id, string chapterId = "", int number = -1, IFormFile? file = null);
+        public Task<bool> Delete(string id);
     }
 }
---
+++ Services/PageService.cs
@@ -0,0 +1,40 @@
+﻿using MangaHomeService.Models;
+using MangaHomeService.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace MangaHomeService.Services
+{
+    public class PageService : IPageService
+    {
+        private readonly IDbContextFactory<MangaHomeDbContext> _contextFactory;
+        public PageService(IDbContextFactory<MangaHomeDbContext> contextFactory) 
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<Page> Add(string chapterId, int number, IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Page> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Page>> GetByChapter(string chapterId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Page> Update(string id, string chapterId = "", int number = -1, IFormFile? file = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
